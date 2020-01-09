b7ef4f68736a6eaab9ba3e3eed91c2f6756776a6
Steve Vance <steve.vance@hotmail.co.uk>
2020-01-09 16:18:22 +0000

    saving data for title author and category completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleSubmit);
+
+  // const formSelectOption = document.querySelector('#new-item-form');
+  // formSelectOption.addEventListener('change', handleSelect);
+});
+
+const handleSubmit = function (event) {
+  event.preventDefault();
+  // console.log("Submitting:", event);
+  const result = document.querySelector('#reading-list')
+  result.textContent = `Book Title: ${event.target.title.value} and Author Name: ${event.target.author.value
+} and Category: ${event.target.category.value}`
+}
+// const handleSelect = function(event) {
+//   // console.log("Submitting", event)
+//   const resultCategory = document.querySelector('#reading-list')
+//   resultCategory.textContent = event.target.value
+
+
+
+
+
+// const handleTitleForm = function(event) {
+//   event.preventDefault()
+//   const resultParagraph = document.querySelector('#title')
+//   resultParargraph.textContent = event.target.title.value;
+  // console.log(event)
